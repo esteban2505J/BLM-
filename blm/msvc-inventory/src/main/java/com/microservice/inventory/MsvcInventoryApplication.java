f781c086a2984b323085ca248b88f7e6ed64319c
esteban2505J <juane.ramirezt@uqvirtual.edu.co>
2025-01-02 14:07:00 -0500

    build(msvc, user,inventory,order,billing): microservices were created

---
+++ blm/msvc-inventory/src/main/java/com/microservice/inventory/MsvcInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcInventoryApplication.class, args);
+	}
+
+}
