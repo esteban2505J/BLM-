37b321a1867e4279b47b2384308c1f7d9d331b73
esteban2505J <juane.ramirezt@uqvirtual.edu.co>
2025-01-02 18:28:03 -0500

    feat: add PostgreSQL connection configuration

---
+++ blm/msvc-user/src/main/java/com/microservice/user/persitence/model/entities/User.java
@@ -0,0 +1,30 @@
+package com.microservice.user.persitence.model.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Data
+@Entity
+@Builder
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
---
+++ blm/msvc-user/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+-- Insertar datos en la tabla `users`
+INSERT INTO users (username, email, password) VALUES ('admin', 'admin@example.com', 'password123');
+
+-- Verificar los datos insertados
+SELECT * FROM users;
