a1d54140aa26ef1ab4f40d4800d9330005148da1
esteban2505J <juane.ramirezt@uqvirtual.edu.co>
2025-01-04 21:24:58 -0500

    chore(properties.yml)

--- blm/msvc-user/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
--- Insertar datos en la tabla `users`
-INSERT INTO users (username, email, password) VALUES ('admin', 'admin@example.com', 'password123');
-
--- Verificar los datos insertados
-SELECT * FROM users;
